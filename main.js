ea26bee1315b751143987d47c003723ebcfc02f0
Renee Doll <renee@reneedoll.com>
2022-10-07 10:31:53 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const createWoodBlock = () => {
+	return {
+		type: "wood block",
+		length: 10,
+		material: "pine",
+		purpose: "flute"
+	}
+
+}
+
+const createBeautifulCarving = (woodObject) => {
+    // Return a string representation of the object
+	const stringRepresentation = `The ${woodObject.length} inch, ${woodObject.material} ${woodObject.type} was carved into a ${woodObject.purpose}`
+
+	return stringRepresentation
+}
+
+// The function returns a value, so that value is stored in a variable
+const woodBlock = createWoodBlock()
+
+// The function returns a value, so that value is stored in a variable
+const carvingString = createBeautifulCarving(woodBlock)  // <-- Missing an argument. Fix it.
+
+console.log(carvingString)
